55819498cb8d64f8fe63a8c8654348aa2e29097e
Eduardo <eduardo@Eduardos-MacBook-Pro.local>
2024-12-12 12:40:20 -0500

    Add maps functionality (Android restriction on cloud with SHA-1)

--- app/src/main/java/com/educhuks/citymapp/ui/composables/MapView.kt
+++ app/src/main/java/com/educhuks/citymapp/ui/composables/MapView.kt
@@ -1,21 +1,36 @@
 package com.educhuks.citymapp.ui.composables
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+
+//-12.149341,-77.0208139
 
 @Composable
 fun MapView(
     item: String,
     modifier: Modifier = Modifier.fillMaxSize()
 ) {
-    Box(
+    val lat = -12.149341
+    val lon = -77.0208139
+
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(LatLng(lat, lon), 10f)
+    }
+
+    GoogleMap(
         modifier = modifier,
-        contentAlignment = Alignment.Center
+        cameraPositionState = cameraPositionState
     ) {
-        Text(text = "Detail of City $item")
+        Marker(
+            state = MarkerState(position = LatLng(lat, lon)),
+            title = "Detail of City $item"
+        )
     }
 }
